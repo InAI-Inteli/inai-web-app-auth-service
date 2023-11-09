5195df831d86bf0493c4a5a9b8bd85bba6dc4a5f
VictorGM01 <victormarques8801@gmail.com>
2023-11-09 08:46:38 -0300

    feat: lista cargos do usuário

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -8,6 +8,7 @@ router
   .get('/usuarios', UsuarioController.listarUsuarios)
   .get('/usuarios/:id', UsuarioController.buscarUsuario)
   .get('/usuarios/:id/diretorias', UsuarioController.listarDiretorias)
+  .get('/usuarios/:id/cargos', UsuarioController.listarCargos)
   .put('/usuarios/:id', validaDTO('usuarios'), UsuarioController.atualizarUsuario)
   .post('/usuarios/:id/alterar-status', UsuarioController.alterarStatus)
   .post('/usuarios', validaDTO('usuarios'), UsuarioController.cadastrarUsuario);
